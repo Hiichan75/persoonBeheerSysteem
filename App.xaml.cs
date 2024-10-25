0254b4ecfc78727c6ab11e1c5b98f7ee07c01015
Hiichan75 <kimsojung19950712@gmail.com>
2024-10-25 17:37:25 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics.Metrics;
+using System.Windows;
+
+namespace persoonBeheerSysteem
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt=False;Trust Server Certificate=False;Application Intent = ReadWrite; Multi Subnet Failover=False");
+        }
+      
+    }
+
+}
